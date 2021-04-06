1a67474f87ab3024c8be9c7b249b009921fed65d
unknown <qokkop@ajou.ac.kr>
2021-04-07 01:14:09 +0900

    fifth commit

--- src/Game.java
+++ src/Game.java
@@ -1,18 +1,20 @@
 
 public class Game {
-	private int score = 0;
+	
 	private int rolls[] = new int[21];
 	private int currentRoll;
 
 	public void roll(int pins) {
-		score += pins;
 		rolls[currentRoll++] = pins;
 	}
 
 	public int score() {
 		int score = 0;
-		for(int i=0; i<rolls.length; i++)
-			score += rolls[i];
+		int i = 0;
+		for(int frame=0; frame<10; frame++) {
+			score += rolls[i] + rolls[i+1];
+			i += 2;
+		}
 		return score;
 	}
 }
